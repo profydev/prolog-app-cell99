bfdfb61caf47a70aefea8f949834193657035010
Mike Cho <cell99@gmail.com>
2024-08-20 14:52:36 +0800

    completed ui checkbox

--- features/ui/checkbox/checkbox.tsx
+++ features/ui/checkbox/checkbox.tsx
@@ -1,19 +1,42 @@
 import classNames from "classnames";
 import styles from "./checkbox.module.scss";
+import { useRef, useEffect } from "react";
 
-type CheckBoxProps = React.InputHTMLAttributes<HTMLInputElement>;
+export enum CheckboxSize {
+  sm = "sm",
+  md = "md",
+}
+
+// type CheckboxProps = React.InputHTMLAttributes<HTMLInputElement>
+
+type CheckboxProps = {
+  children?: React.ReactNode;
+  size?: CheckboxSize;
+  checked?: boolean;
+  disabled?: boolean;
+  indeterminate?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
 export function CheckBox({
   children,
-  className,
-  style,
-  ...otherprops
-}: CheckBoxProps) {
+  size = CheckboxSize.sm,
+  indeterminate = false,
+  ...props
+}: CheckboxProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      /* toggle values*/
+      inputRef.current.indeterminate = indeterminate;
+    }
+  }, [indeterminate]);
+
   return (
-    <label className={classNames(styles.container, className)} style={style}>
-      <input {...otherprops} type="checkbox" />
-      <span className={styles.checkbox}>kjh</span>
-      {children}
+    <label className={classNames(styles[size], styles.checkboxWrapper)}>
+      <input {...props} type="checkbox" ref={inputRef} />
+      {children ? <span>{children}</span> : ""}
     </label>
   );
 }
